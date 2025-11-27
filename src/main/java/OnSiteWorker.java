5aaff7084b903ef5a344edf78275602cade4d214
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-27 08:47:51 +0100

    feat: created OnSiteWorker class

--- src/main/java/OnSiteWorker.java
+++ src/main/java/OnSiteWorker.java
@@ -1,8 +1,37 @@
 public class OnSiteWorker extends Worker {
 
-
+    private static double petrol;
+    
     public OnSiteWorker(String name, String surname, double hourPrice) {
         super(name, surname, hourPrice);
+    }
+
+    @Override
+    public double calculateSalary(int hoursWorked) {
+        return (getHourPrice() * hoursWorked) + petrol;
+    }
+
+    public static double getPetrol() {
+        return petrol;
+    }
+
+    public static void setPetrol(double petrol) {
+        OnSiteWorker.petrol = petrol;
+    }
+
+    @Deprecated
+    public double calculateSalaryOld(int hoursWorked) {
+        return getHourPrice() * hoursWorked;
+    }
+
+    @Override
+    public String toString() {
+        return "OnSiteWorker{" +
+                "name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", hourPrice=" + getHourPrice() +
+                ", petrol=" + petrol +
+                '}';
     }
 
 
